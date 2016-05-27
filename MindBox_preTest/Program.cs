fa42f7e446f48cf7c1b2fbcda85ea9ccdc501954
yaternovskiy <ya.ternovskiy@gmail.com>
2016-05-27 09:08:56 +0600

    Finished with unit tests

---
+++ MindBox_preTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MindBox_preTest
+{
+    public class Calculator
+    {
+        private const float AllowedDeviation = 0.01F;
+
+        static void Main(string[] args)
+        {
+        }
+
+        static public double TriangleArea3Sides(params double[] args)
+        {
+            Array.Sort(args);
+
+            if (!IsValidTriangle(args)) {
+                Console.Write("Invalid parameters for TriangleArea3Sides. 3 sides' lengths are needed.");
+                return 0;
+            };
+            
+            return args[0] * args[1] / 2;
+        }
+
+        // Следующий метод я бы сделал приватным и юнит-тесты для него не делал бы, но здесь сделаю публичным...
+
+        static public bool IsValidTriangle(double[] args) // checks if valid lengths of triangle's sides are given
+        {
+            if (args.Length != 3) return false;     // total 3 values accepted
+
+            foreach (double value in args)          // positive values accepted
+            {
+                if (value <= 0) return false;
+            }
+
+            // Pifagor's theorem check with given allowed deviation
+            if (Math.Abs((Math.Pow(args[2], 2) / (Math.Pow(args[0], 2) + Math.Pow(args[1], 2)) - 1)) > AllowedDeviation)
+                return false;
+            else return true;
+
+        }
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MindBox_preTest;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestsCalculation
+    {
+        // Here I have to somehow mock the static constant and validation method. But I'll do it later.
+
+        [TestMethod]
+        public void AreaCalcCorrect() // runs with different args' types
+        {
+            var actual = Calculator.TriangleArea3Sides(3, 4.0F, 5.0);
+            Assert.AreEqual(6.0, actual);
+        }
+
+        [TestMethod]
+        public void AreaCalcFail() // returns 0 if validation fail
+        {
+            var actual = Calculator.TriangleArea3Sides(0, 4.0F, 5.0);
+            Assert.AreEqual(0, actual);
+        }
+    }
+
+    [TestClass]
+    public class UnitTestsValidation
+    {
+        [TestMethod]
+        public void TestTriangleValidationOK() // Valid, OK and corresponds to Pifagor rule with given deviation
+        {
+            var values = new double[] { 1.0, 2.0, 2.23 };
+            var actual = Calculator.IsValidTriangle(values);
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod]
+        public void TestTriangleValidationFail1() // Invalid: 0 arguments
+        {
+            var values = new double[0];
+            var actual = Calculator.IsValidTriangle(values);
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void TestTriangleValidationFail2() // Invalid: 4 arguments
+        {
+            var values = new double[0];
+            var actual = Calculator.IsValidTriangle(values);
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void TestTriangleValidationFail3() // Invalid: negative value
+        {
+            var values = new double[] { 1.0, -2.0, 2.23 };
+            var actual = Calculator.IsValidTriangle(values);
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void TestTriangleValidationFail4() // Invalid: zero value
+        {
+            var values = new double[] { 1.0, 0, 2.23 };
+            var actual = Calculator.IsValidTriangle(values);
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod]
+        public void TestTriangleValidationFailPifagor() // Invalid: Pifagor rule
+        {
+            var values = new double[] { 1.0, 2.0, 2.1 };
+            var actual = Calculator.IsValidTriangle(values);
+            Assert.AreEqual(false, actual);
+        }
+    }
+}
